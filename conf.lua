4370be065f3e36a559c883abba3ecb92f0512b87
sacemakesgame <100522891+sacemakesgame@users.noreply.github.com>
2025-07-24 13:10:39 +0700

    turn off console

--- conf.lua
+++ conf.lua
@@ -26,7 +26,8 @@ height = 1080
 function love.conf(t)
     t.identity = nil                   -- The name of the save directory (string)
     t.version = '11.4'                 -- The LÖVE version this game was made for (string)
-    t.console = true                  -- Attach a console (boolean, Windows only)`
+    -- t.console = true                  -- Attach a console (boolean, Windows only)`
+    t.console = false                  -- Attach a console (boolean, Windows only)`
 
     t.window.title = 'tiny boatball!' -- The window title (string)
     t.window.icon = nil                -- Filepath to an image to use as the window's icon (string)
